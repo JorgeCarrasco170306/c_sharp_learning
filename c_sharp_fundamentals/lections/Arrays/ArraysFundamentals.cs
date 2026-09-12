2da99fed11e6dea0437b65fad6785fb032080bc7
jorge <jorgedev@hotmail.com>
2026-09-12 07:55:24 -0400

    Fix array initialization syntax and enhance Run method to display array elements

--- c_sharp_fundamentals/lections/Arrays/ArraysFundamentals.cs
+++ c_sharp_fundamentals/lections/Arrays/ArraysFundamentals.cs
@@ -2,5 +2,25 @@ namespace c_sharp_fundamentals.lections.Arrays;
 
 public class ArraysFundamentals
 {
+    public static void Run()
+    {
+        int[] numbers = [1, 2, 3, 4, 5];
+        int[] numbers1 = new int[5];
+        int[] numbers2 = { 3, 2, 1 };
 
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        foreach (int number in numbers1)
+        {
+            Console.WriteLine(number);
+        }
+
+        foreach (int number in numbers2)
+        {
+            Console.WriteLine(number);
+        }
+    }
 }
