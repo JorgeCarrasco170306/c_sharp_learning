e1e7c47c2abf04d9cd3e1e3523e031deda9b2647
jorge <jorgedev@hotmail.com>
2026-09-12 08:30:46 -0400

    Add TwoDimensionArray class with Run method for 2D and 3D array initialization

---
+++ c_sharp_fundamentals/lections/Arrays/TwoDimensionArray.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace c_sharp_fundamentals.lections.Arrays
+{
+    public class TwoDimensionArrray
+    {
+        public static void Run()
+        {
+            int[,] array2d = new int[3, 3];
+            int[,,] array3d = new int[3, 3, 3];
+
+
+            int[,] array2dInitialize = { { 1, 2 } ,{ 3, 4 } };
+        }
+    }
+}
